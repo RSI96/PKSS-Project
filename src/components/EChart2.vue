cad4ad2df6ca722362f44ee5c7970adff5a7d7c0
radek.sikora <radek.sikora@bitcave.eu>
2019-12-10 16:22:13 +0100

    final corrections

--- src/components/EChart2.vue
+++ src/components/EChart2.vue
@@ -3,46 +3,21 @@
         <div class="echarts">
             <chart :options="chartOptionsBar"/>
         </div>
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Data od" aria-label="Data od" aria-describedby="basic-addon1" v-model="data_od">
-            <input type="text" class="form-control" placeholder="Data do" aria-label="Data do" aria-describedby="basic-addon1" v-model="data_do">
-            <select class="custom-select" id="inputGroupSelect02" v-model="parametr">
-                <option selected>Wybierz parametr</option>
-                <option value="U_b">U_b</option>
-                <option value="F_cob">F_cob</option>
-                <option value="T_h">T_h</option>
-                <option value="T_h">T_h</option>
-                <option value="T_rref">T_rref</option>
-                <option value="T_r">T_r</option>
-                <option value="T_pcob">T_pcob</option>
-            </select>
-            <div class="input-group-append">
-                <button class="btn btn-outline-primary" type="button" @click="downloadData">Akceptuj</button>
-            </div>
-        </div>
     </div>
 </template>
 
 <script>
-    import axios from "axios";
-
     export default {
-        name: "EChart",
-        methods: {
-            downloadData() {
-                axios.get("http://ADRES:8080/history" + '?data_od=' +this.data_od + '&data_do=' +this.data_do + '&parametr=' +this.parametr)
-                    .then(res => (this.history = res.data));
-                }
+        name: "EChart2",
+        props: {
+            history: Object
         },
+
         data() {
             return {
-                history: null,
-                data_od: null,
-                data_do: null,
-                parametr: null,
                 chartOptionsBar: {
                     title: {
-                        text: "Temperatura w budynku w ciągu dnia",
+                        text: "Temperatura w budynku",
                         x: 'center',
                         textStyle: {
                             fontSize: 18
@@ -86,7 +61,8 @@
                     color: '#428bca'
                 }
             }
-        }
+        },
+
     }
 </script>
 
--- src/views/Building2.vue
+++ src/views/Building2.vue
@@ -10,35 +10,66 @@
         <div class="row">
             <div class="col-sm">
                 <app-chart
-                        v-for="reading in history"
-                        :key="reading.id"
-                        :reading="reading"
+                :history="history[0]"
                 />
-            </div>
 
+            </div>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Data od" aria-label="Data od" aria-describedby="basic-addon1" v-model="data_od">
+                <input type="text" class="form-control" placeholder="Data do" aria-label="Data do" aria-describedby="basic-addon1" v-model="data_do">
+                <select class="custom-select" id="inputGroupSelect02" v-model="parametr">
+                    <option selected>Wybierz parametr</option>
+                    <option value="U_b">U_b</option>
+                    <option value="F_cob">F_cob</option>
+                    <option value="T_h">T_h</option>
+                    <option value="T_h">T_h</option>
+                    <option value="T_rref">T_rref</option>
+                    <option value="T_r">T_r</option>
+                    <option value="T_pcob">T_pcob</option>
+                </select>
+                <div class="input-group-append">
+                    <button class="btn btn-outline-primary" type="button" @click="downloadData">Akceptuj</button>
+                </div>
+            </div>
+        </div>
+        <div>
         </div>
     </div>
 </template>
 
 <script>
-    import EChart from "../components/EChart";
+    import EChart2 from "../components/EChart2";
     import Measurement from "../components/Measurement";
     import axios from "axios"
 
     export default {
-        name: "Building3",
+        name: "Building2",
         components: {
-            appChart: EChart,
+            appChart: EChart2,
             appMeasurement: Measurement
         },
         data() {
             return {
                 actual: null,
+                history: null,
+                data_od: null,
+                data_do: null,
+                parametr: null,
+            }
+        },
+        methods: {
+            downloadData() {
+                axios.get("http://4bcc0e78.ngrok.io/budynek2/" + this.parametr + "?timestamp_begin=" + this.data_od + "&timestamp_end=" + this.data_do)
+                    .then(res => (this.history = res.data));
+                return true
             }
         },
         created() {
-            axios.get("http://10.129.11.171:8080/actual")
+            axios.get("http://4bcc0e78.ngrok.io/budynek2")
                 .then(res => (this.actual = res.data));
+            axios.get("http://4bcc0e78.ngrok.io/budynek2/U_b?timestamp_begin=1999-01-07&timestamp_end=2019-12-30")
+                .then(res => (this.history = res.data));
+
         }
     }
 </script>
